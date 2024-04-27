d9026401e298ab8954ebcc944dcfcc2cda00620e
Solmelon <sol5415@gmail.com>
2024-04-27 14:31:20 +1000

    added functionality for disorientating vines

--- Assets/Scripts/Platformer/Platformer2DUserControl.cs
+++ Assets/Scripts/Platformer/Platformer2DUserControl.cs
@@ -12,16 +12,30 @@ namespace UnityStandardAssets._2D
         private bool m_SustainJump;
         private bool m_DisableControl;
         private bool m_Dash;         // DASH UPDATE
+        private float reverseTimer;
+        public float reverseSpan = 5;
+        public bool reverse;
 
 
         private void Awake()
         {
             m_Character = GetComponent<PlatformerCharacter2D>();
+            reverseTimer = reverseSpan;
         }
 
 
         private void Update()
         {
+
+            if (reverse) {
+                if (reverseTimer > 0) {
+                    reverseTimer -= Time.deltaTime;
+                    return;
+                }
+                reverse = false; 
+                GetComponent<SpriteRenderer>().color = Color.white;
+                reverseTimer = reverseSpan;
+            }
             // If control is disabled, return.
             if (m_DisableControl) return;
 
@@ -37,7 +51,13 @@ namespace UnityStandardAssets._2D
                 m_Dash = CrossPlatformInputManager.GetButtonDown("Fire3");
             }
         }
-
+        private void reverseTrue() //player receives reverseTrue message from disorientating vines
+        {
+            if (!reverse && reverseTimer > 0) {
+            reverse = true;
+            GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
 
         private void FixedUpdate()
         {
@@ -47,6 +67,9 @@ namespace UnityStandardAssets._2D
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            if (reverse) {
+                h = -CrossPlatformInputManager.GetAxis("Horizontal");  //when reverse is true, horizontal inputs go in opposite directions
+            }
             // DASH UPDATED - Move function was updated in PlatformerCharacter2D with the dash variable as the 5th argument).
             m_Character.Move(h, crouch, m_Jump, m_SustainJump, m_Dash);
             m_Jump = false;
